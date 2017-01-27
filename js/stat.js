cb275d2e42f79efb52a6e60d8fd62420feff01d4
idu135 <marsha85@mail.ru>
2017-01-27 22:32:31 +0300

    исправвила гистограмму

--- js/stat.js
+++ js/stat.js
@@ -1,20 +1,31 @@
-'use strict'
+'use strict';
 
 window.renderStatistics = function(ctx, names, times){
-	ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-	ctx.fillRect(110, 20, 420, 270);
 
-	ctx.fillStyle = '#fff';
-	ctx.fillRect(100, 10, 420, 270);
+	var drawText = function(text, x, y, color){
+		ctx.fillStyle = color || '#000';
+		ctx.font = '16px PT Mono';
+		ctx.fillText(text, x, y);
+	};
+
+	var drawRect = function(x1, y1, x2, y2, color){
+		ctx.fillStyle = color;
+		ctx.fillRect(x1, y1, x2, y2);
+	};
+
+	var randomColor = function(){
+	  return 'rgba(0, 0, ' + (Math.random() * 255).toFixed(0) + ', ' + (Math.random() * 0.5).toFixed(1) + ')';
+	}
+
+	drawRect(110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)');
+
+	drawRect(100, 10, 420, 270, '#FFF');
 
 	ctx.strokeStyle = '#000';
 	ctx.strokeRect(100, 10, 420, 270);
 
-
-	ctx.fillStyle = "#000"
-	ctx.font = '16px PT Mono #000';
-	ctx.fillText('Ура, вы победили!', 110, 40);
-  ctx.fillText('Список результатов:', 110, 55);
+	drawText('Ура, вы победили!', 110, 40);
+  drawText('Список результатов:', 110, 55);
 
   var max = 0;
   for (var i = 0; i < times.length; i ++) {
@@ -24,7 +35,7 @@ window.renderStatistics = function(ctx, names, times){
       }
   }
 		var histoHeight = 150,
-    histoX = 110;
+    histoX = 110,
 	  columnWidth = 40,
 	  columnIndent = 90,
 	  step = histoHeight / max;
@@ -33,21 +44,14 @@ window.renderStatistics = function(ctx, names, times){
     	var name = names[i];
       var time = times[i];
       var height = step * time;
+			var currentColor = '#FF0000';
 
-      if(name === 'Вы'){
-      ctx.fillStyle = 'red';
-      } else {
-      ctx.fillStyle = 'rgba(0, 0, ' + (Math.random() * 255).toFixed(0) + ', ' + (Math.random() * 0.5).toFixed(1) + ')'
-
-      var max = 255, min = 1;
+      if(name !== 'Вы'){
+      	currentColor = randomColor();
+      	var max = 255, min = 1;
       }
 
-      ctx.fillRect(histoX + columnIndent * i, 200, columnWidth, -height);
-      ctx.fillStyle = '#000';
-      ctx.fillText(name + ' : ' + time.toFixed(0), histoX + columnIndent * i, histoHeight+ 80);
+      drawRect(histoX + columnIndent * i, 200, columnWidth, -height, currentColor);
+      drawText(name + ' : ' + time.toFixed(0), histoX + columnIndent * i, histoHeight+ 80);
     }
 }
-
-var canvas = document.querySelector('canvas');
-
-renderStatistics(canvas.getContext('2d'), ['Вы', 'Катя', 'Люся', 'Аня'], [100, 220, 140, 300]);
